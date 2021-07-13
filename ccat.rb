83d2aabc9bc06db38bd71963c8e29c4ec9d3108a
tknb_023 <tknb.bsn.2975@gmail.com>
2021-07-13 14:47:42 +0900

    sha256の更新

--- ccat.rb
+++ ccat.rb
@@ -5,7 +5,7 @@ class Ccat < Formula
   homepage "https://github.com/tknb-023/ccat"
   url "https://github.com/tknb-023/ccat/releases/download/v#{VERSION}/ccat-#{VERSION}_darwin_amd64.tar.gz"
   version VERSION
-  sha256 "02f134076d309ffebd309f6936bd7996b3c07e35b7b80318d7aa6c6296c8cd80"
+  sha256 "7d45b00ce61586947ad0ebce590fe20a1638c22acad4dfca53ea7758a13153ff"
   license "MIT LICENSE"
   
   def install
